7f4c1f4bc99bdd94a3e9bb7d5afb19d43bbb8470
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-06-08 14:01:22 +0100

    Update 2024_05_08_153926_create_file_table.php

--- backend/database/migrations/2024_05_08_153926_create_file_table.php
+++ backend/database/migrations/2024_05_08_153926_create_file_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('discussion_id')->nullable()->constrained()->onDelete('cascade');
-            $table->foreignId('message_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('path');
             $table->string('size');
