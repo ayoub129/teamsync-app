25044447bd63a3ad09f56982603f8cbdf7e7f955
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-06-09 15:13:55 +0100

    Update AuthController.php

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -61,7 +61,7 @@ class AuthController extends Controller
         ]);
 
         // Create user in local database
-        $user = \App\Models\User::create([
+        $user = User::create([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => Hash::make($request->input('password')),
