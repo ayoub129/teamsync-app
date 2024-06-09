a09844417f3addfae33d8c43466b9625981bdbc2
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-06-09 17:14:43 +0100

    Update Message.php

--- backend/app/models/Message.php
+++ backend/app/models/Message.php
@@ -13,11 +13,11 @@ class Message extends Model
 
     public function sender()
     {
-        return $this->belongsTo(User::class, 'sender_id');
+        return $this->belongsTo(Users::class, 'sender_id');
     }
 
     public function receiver()
     {
-        return $this->belongsTo(User::class, 'receiver_id');
+        return $this->belongsTo(Users::class, 'receiver_id');
     }
 }
