cf6da165f48ee2bcc379d46b1f74d947185f0d08
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-06-10 16:03:37 +0100

    Create Channel.php

---
+++ backend/app/Models/Channel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Channel extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'status'];
+
+    public function discussions()
+    {
+        return $this->hasMany(Discussion::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'channel_users')->withTimestamps();
+    }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class, 'channel_groups')->withTimestamps();
+    }
+
+    public function getDiscussionCountAttribute()
+    {
+        return $this->discussions()->count();
+    }
+
+    public function getUserCountAttribute()
+    {
+        return $this->users()->count();
+    }
+
+    public function getGroupCountAttribute()
+    {
+        return $this->groups()->count();
+    }
+
+    public function getMessageCountAttribute()
+    {
+        return $this->messages()->count();
+    }
+}
