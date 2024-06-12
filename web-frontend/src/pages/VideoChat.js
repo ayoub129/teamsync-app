f49b663bf66d4aff71b166181a2d52f13bfd10ae
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-06-12 20:33:19 +0100

    Update VideoChat.js

--- web-frontend/src/pages/VideoChat.js
+++ web-frontend/src/pages/VideoChat.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect , useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/ui/Header';
 import SideBar from '../components/ui/SideBar';
 import Video from '../components/chat/Video';
 
 const VideoChat = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -12,12 +13,17 @@ const VideoChat = () => {
     if (!token) {
       navigate("/login");
     }
+
   }, [navigate]);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <div className='min-h-screen'>
-      <Header />
-      <SideBar active="video" />
+      <Header toggleSidebar={toggleSidebar} />
+      <SideBar active="video" isSidebarOpen={isSidebarOpen} />
       <Video />
     </div>
   );
