fc4c526544d885642a5f4e88a499e7b7a8c743e0
ayoubtest1 <techshop000.store@gmail.com>
2024-06-13 15:21:51 +0100

    add messaging and fix cors again !!

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -2,41 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Message;
-use App\Events\MessageSent;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
-    public function sendMessage(Request $request)
+    public function index()
     {
-        $request->validate([
-            'receiver_id' => 'required|exists:users,id',
-            'message' => 'required|string',
-        ]);
-
-        $message = Message::create([
-            'sender_id' => Auth::id(),
-            'receiver_id' => $request->receiver_id,
-            'message' => $request->message,
-        ]);
-
-        broadcast(new MessageSent($message))->toOthers();
-
-        return response()->json(['message' => $message], 200);
+        return Message::with(['sender', 'receiver'])->get();
     }
 
-    public function getMessages($receiverId)
+    public function store(Request $request)
     {
-        $messages = Message::where(function ($query) use ($receiverId) {
-            $query->where('sender_id', Auth::id())
-                  ->orWhere('receiver_id', Auth::id());
-        })->where(function ($query) use ($receiverId) {
-            $query->where('sender_id', $receiverId)
-                  ->orWhere('receiver_id', $receiverId);
-        })->get();
+        $message = DB::transaction(function () use ($request) {
+            $message = Message::create($request->all());
+            event(new \App\Events\MessageSent($message));
+            return $message;
+        });
 
-        return response()->json(['messages' => $messages], 200);
+        return response()->json($message, 201);
     }
 }
