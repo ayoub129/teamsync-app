ee12a7d8dc4997f865b4cee7e834ba1e7ed8326e
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2024-06-14 11:49:10 +0100

    Update axios.js

--- web-frontend/src/axios.js
+++ web-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://54.242.152.138:8000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://54.208.42.53:8000/api',
 });
 
 export default instance;
