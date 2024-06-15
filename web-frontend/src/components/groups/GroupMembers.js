b8dff0c2c55daf9030f5017d58fa989d56f57ae1
ayoubtest1 <techshop000.store@gmail.com>
2024-06-15 18:05:17 +0100

    group edit

--- web-frontend/src/components/groups/GroupMembers.js
+++ web-frontend/src/components/groups/GroupMembers.js
@@ -99,7 +99,7 @@ const GroupMembers = ({ groupId }) => {
         members.map(member => (
           <div key={member.id} className="flex items-center justify-between p-2 mb-2 bg-white rounded shadow">
             <div className="flex items-center">
-              <img src={member.image} alt={member.name} className="w-10 h-10 rounded-full mr-3" />
+              <img src={member.image || "https://images.unsplash.com/photo-1581382575275-97901c2635b7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={member.name} className="w-10 h-10 rounded-full mr-3" />
               <span>{member.name}</span>
             </div>
             {member.id !== parseInt(userId , 10) && (
