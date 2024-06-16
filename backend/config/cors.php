613294611d22a2311aee10e0f041d3278a381fc8
ayoubtest1 <techshop000.store@gmail.com>
2024-06-16 15:41:37 +0100

    change ip

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://54.208.42.53' , 'http://54.208.42.53/' ],
+    'allowed_origins' => ['http://54.208.42.53'],
 
     'allowed_origins_patterns' => [],
 
