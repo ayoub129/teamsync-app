9786e40d25bbc0d30469cd6f4a583e8215ace8ef
ayoubtest1 <techshop000.store@gmail.com>
2024-06-18 12:37:41 +0100

    Ch

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://34.229.66.208' , 'https://34.229.66.208:3000'],
+    'allowed_origins' => ['http://34.229.66.208'],
 
     'allowed_origins_patterns' => [],
 
