51354d4be05a2acaac1b475e4a5eb43e009abba7
ayoubtest1 <techshop000.store@gmail.com>
2024-06-19 01:04:06 +0100

    update chat

--- web-frontend/src/components/chat/MessageArea.js
+++ web-frontend/src/components/chat/MessageArea.js
@@ -4,7 +4,7 @@ const MessageArea = ({ messages, onSendMessage, newMessage, setNewMessage }) => 
   return (
     <div className="w-2/3 p-4 bg-white shadow-lg flex flex-col h-[80%] overflow-y-scroll">
       <div className="flex-grow overflow-y-auto mb-4">
-        {messages.map((message) => (
+        {messages && messages.map((message) => (
           <div
             key={message.id}
             className={`p-2 mb-3 ${message.sender_id === parseInt(localStorage.getItem('user_id')) ? 'bg-blue-100' : 'bg-gray-100'} rounded`}
--- web-frontend/src/pages/Chat.js
+++ web-frontend/src/pages/Chat.js
@@ -65,7 +65,6 @@ const Chat = () => {
   
       const pusher = new Pusher(process.env.REACT_APP_PUSHER_APP_KEY, {
         cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER,
-        authEndpoint: `${process.env.REACT_APP_API_URL}/broadcasting/auth`,
         auth: {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
