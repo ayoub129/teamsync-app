7b5c23057650b6c0d2548a234eeae9a9cb18787d
ayoubtest1 <techshop000.store@gmail.com>
2024-06-24 15:55:54 +0100

    update the messaging

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://18.204.6.186', 'http://ec2-18-204-6-186.compute-1.amazonaws.com'],
+    'allowed_origins' => ['http://18.204.6.186/', 'http://18.204.6.186'],
 
     'allowed_origins_patterns' => [],
 
