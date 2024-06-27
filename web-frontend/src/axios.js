9cba1f81dd3f1a46a58c10584654bf7920f0c24b
ayoubtest1 <techshop000.store@gmail.com>
2024-06-27 09:27:49 +0100

    nginx

--- web-frontend/src/axios.js
+++ web-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://34.232.72.99:8000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://nanachat.online/api',
 });
 
 export default instance;
